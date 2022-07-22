5912aa3e03e347f92b95e45d8e5961e514ef50d6
Cesar Gonzalez <gcesar13@yahoo.com>
2022-07-22 16:18:38 -0600

    start of code and sql tables

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+-- Tables
+
+CREATE TABLE departments (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id: INTEGER PRIMARY KEY,
+    title: VARCHAR(30),
+    salary: DECIMAL,
+    department_id: INTEGER
+);
+
+CREATE TABLE employee (
+    id: INTEGER PRIMARY KEY,
+    first_name: VARCHAR(30),
+    last_name: VARCHAR(30),
+    role_id: INTEGER,
+    manager_id: INTEGER DEFAULT NULL,
+    CONSTRAINT fk_roles Foreign KEY REFERENCES roles(department_id)
+);
